e1bef2ef31f1a75c9c001549a4e04849bf474938
pr <rvpspran@gmail.com>
2023-05-21 04:16:10 +0530

    finalSample

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -11,8 +11,8 @@
         <nav class="nav-show">
       <ul>
         <li><a href="#">Home</a></li>
-        <li><a href="#about-us">About Us</a></li>
-        <li><a href="#contact-us">Contact Us</a></li>
+        <li><a href="#about-us">About</a></li>
+        <li><a href="#contact-us">Contact</a></li>
         <li ><router-link to="/Login">Login</router-link></li>
       </ul>
       </nav>
